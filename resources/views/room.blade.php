05a0abadab109bf4e9282dde13a7c6e8bd866542
Marie NUELLAS <marie.nuellas@ozitem.com>
2020-08-21 10:25:35 +0200

    tried to add laravel-echo

---
+++ resources/views/room.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Lolitadventure') }}</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+    <style>
+        body {
+            background-color: pink;
+    color: white;
+    font-family: "Xiomara";
+    text-align: center;
+    width: 100%;
+    height: 100vh;
+    font-size: 5vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 0;
+        }
+    </style>
+</head>
+<body>
+<p>Room</p>
+<p><a href="{{ url('/') }}"><i class="fas fa-home"></i></a></p>
+</html>
